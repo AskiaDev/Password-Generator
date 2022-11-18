1243711f0967dddcfaa10287a45f8c10fae09150
Askia3 <itsmejamesmanjares@gmail.com>
2022-11-19 00:25:25 +0800

    i change  some sizes

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 let pass1 = document.getElementById("pass1")
 let pass2 = document.getElementById("pass2")
 let passwordLength = 8
 
-
-
 function generate(){
-    let generatedPasswordOne = randomPassword()
-    let generatedPasswordTwo = randomPassword()
-    pass1.textContent = generatedPasswordOne
-    pass2.textContent = generatedPasswordTwo
+    let generatePasswordOne = randomPassword()
+    let generatePasswordTwo = randomPassword()
+    pass1.textContent = generatePasswordOne
+    pass2.textContent = generatePasswordTwo
 }
 
 function generatePass(){
@@ -19,7 +17,7 @@ function generatePass(){
     return characters[randomChar]
 }
 
-function randomPassword() {
+function randomPassword(){
     let randomPass = ""
     for (let i = 0; i < passwordLength; i++){
         randomPass += generatePass()
@@ -32,7 +30,7 @@ function generatePass(){
     return characters[randomChar]
 }
 
-function randomPassword() {
+function generatePassword(){
     let randomPass = ""
     for (let i = 0; i < passwordLength; i++){
         randomPass += generatePass()
